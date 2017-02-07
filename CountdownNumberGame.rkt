ded79181ada06f9e7c126f6664df921e7b95b692
paddygriffin <paddygriffin1995@gmail.com>
2017-02-07 18:03:01 +0000

    Create List of numbers

--- CountdownNumberGame.rkt
+++ CountdownNumberGame.rkt
@@ -1,6 +1,8 @@
 #lang racket
+
 ;Patrick Griffin G00314635
 ;Theory of Algorithms Project
 ;Countdown Number Game
 
-
+;list of available numbers for the game, can only get 6 of these 
+(define listNums (list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 25 50 75 100))
