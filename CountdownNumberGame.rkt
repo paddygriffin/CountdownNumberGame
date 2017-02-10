125013818ee5a770376f6211e8d05cd0a30650f9
paddygriffin <paddygriffin1995@gmail.com>
2017-02-10 14:16:07 +0000

    random number and empty list

--- CountdownNumberGame.rkt
+++ CountdownNumberGame.rkt
@@ -6,3 +6,26 @@
 
 ;list of available numbers for the game, can only get 6 of these 
 (define listNums (list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 25 50 75 100))
+listNums
+
+;generator
+(define rand (random 101 1000))
+rand
+
+;define a list for the 6 random numbers to go in (an empty list)
+(define nums(list))
+nums
+
+;Now we have an empty list we have to fill it with 6 random numbers
+;1st- define a function
+;2nd- use cons to add onto list
+;(define gameNums(n))
+
+
+;Found online might help later
+;(define (pick-item l)
+ ; (list-ref l (random (length l))))
+
+;Code to generate random numbers between 2 numbers - Ex - (random-number-list 10 20) - use this as a reference
+;(define (random-number-list n listNums)
+ ; (build-list n (lambda (listNums) (random listNums))))
