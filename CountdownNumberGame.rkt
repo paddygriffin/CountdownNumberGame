fc917afd9f9fd6bd8604ae02e743e24642ca0ec4
paddygriffin <paddygriffin1995@gmail.com>
2017-04-19 11:09:08 +0100

    Working on RPN function
    working on to see if theie is the right amount on the stack and apply
    this to every element on the stack

--- CountdownNumberGame.rkt
+++ CountdownNumberGame.rkt
@@ -36,3 +36,28 @@ rand
 (pick-item listNums)
 |#
 
+; -1 will represent operators
+; 1 will represent numbers. 
+(define start(list -1 -1 -1 -1 1 1 1 1))
+
+;This line creates all permutations and it removes the duplicates 
+(define perms (remove-duplicates (permutations start)))
+
+;This function adds the two 1s to the front of the list and the -1 to the end of the list 
+(define (to-rpn l)
+ (append (list 1 1) l (list -1)))
+
+(map to-rpn perms)
+
+;Apply function to every element of the list to get valid RPN
+;s= stack to see how many on it
+;e= list
+(define (valid-rpn? e (s 0)) ;default value is 0
+  (if (null? e)
+      (if (= s 1) #t #f)
+      (if (= (car e) 1)
+          (valid-rpn? (cdr e) (+ s 1))
+      (if (< s 2)
+          #f
+          (#t))))) ;true ot false
+
--- README.md
+++ README.md
@@ -4,3 +4,9 @@ A countdown number game for theory of algorithmns project using functional progr
 ### Algorithm 
 I used remove-duplicates built in function to remove repeated statements in order
 to make the problem smaller. And then working on making it a valid RPN.
+
+Map adds two numbers to start of all permutations and adds one operator to the end of permutations
+
+Working on checking if a function is valid for RPN and need to apply the function to 
+every element of the list/stack and some equations are still not valid from my code. Also
+remove-duplicates cant get inside nested lists.
