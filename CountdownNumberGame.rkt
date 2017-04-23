73fe0ad2abddbc0f47bf71cae3ab1e5616b7892c
paddygriffin <paddygriffin1995@gmail.com>
2017-04-23 20:13:59 +0100

    Second function with 2 numbers
    adapted from website link in code

--- CountdownNumberGame.rkt
+++ CountdownNumberGame.rkt
@@ -38,16 +38,60 @@ rand
 
 ; -1 will represent operators
 ; 1 will represent numbers. 
-(define start(list -1 -1 -1 -1 1 1 1 1 1))
+(define start(list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 25 50 75 100))
+(define op (list '+ '+ '+ '+ '+ '- '- '- '- '- '- '* '* '* '* '*  '/ '/ '/ '/ '/))
+;Give the outline of numbers
+start
+;Generate Random Num
+(define rand (random 101 1000))
+rand
+
+;define a list for the 6 random numbers to go in (an empty list)
+(define nums(list))
+;nums
+
+
+;Now we have an empty list we have to fill it with 6 random numbers
+;1st- define a function
+;2nd- use cons to add onto list
+;(define gameNums(n))
+;list-ref gets random position value from a list 
+;set! saves number
+;https://www.rosettacode.org/wiki/Pick_random_element - adapted from
+
+(define numbers4 (list));Creates list for selecting 4 numbers
+(define (randomListNumbers l);Creates list for random numbers  
+  (define randomNumber(list-ref l(random (length l))))  
+  (set! l(remove randomNumber l)) 
+  (set! numbers4 (cons randomNumber numbers4))  
+  (if (= (length numbers4) 4)
+     numbers4 ;Prints 4 random numbers
+      (randomListNumbers l))
+)
+(randomListNumbers start);Outputs random list
+
+(define numbers2 (list));Creates list for selecting 4 numbers
+(define (listNumbers2 r);Creates list for random numbers  
+  (define randomNumber(list-ref r(random (length r))))  
+  (set! r(remove randomNumber r)) 
+  (set! numbers2 (cons randomNumber numbers2))  
+  (if (= (length numbers2) 2)
+     numbers2 ;Prints 2 random numbers
+      (listNumbers2 r))
+)
+(listNumbers2 start);Outputs second random list
+
+
+#|
 
 ;This line creates all permutations and it removes the duplicates 
 (define perms (remove-duplicates (permutations start)))
 
 ;This function adds the two 1s to the front of the list and the -1 to the end of the list 
-;(define (to-rpn l)
-; (append (list 1 1) l (list -1)))
-;(to-rpn (car  perms))
-;(map to-rpn perms)
+(define (to-rpn l)
+ (append (list 1 1) l (list -1)))
+
+(map to-rpn perms)
 
 ;Apply function to every element of the list to get valid RPN
 ;s= stack to see how many on it
@@ -57,39 +101,8 @@ rand
       (if (= s 1) #t #f)
       (if (= (car e) 1)
           (valid-rpn? (cdr e) (+ s 1))
-      (if (> s 1)
-          (valid-rpn? (cdr e) (- s 1))
-          (#f))))) ;true ot false
+      (if (< s 2)
+          #f
+          (#t))))) ;true ot false
 
-;Calculate RPN adapted from https://rosettacode.org/wiki/Parsing/RPN_calculator_algorithm#Racket
-(define (calculate-rpn expr)
-  (for/fold ([stack '()]) ([token expr])
-    (printf "~a\t -> ~a~N" token stack)
-    (match* (token stack)
-     [((? number? n) s) (cons n s)]
-     [('+ (list x y s ___)) (cons (+ x y) s)]
-     [('- (list x y s ___)) (cons (- y x) s)]
-     [('* (list x y s ___)) (cons (* x y) s)]
-     [('/ (list x y s ___)) (cons (/ y x) s)]
-     [(x s) (error "calculate-RPN: Cannot calculate the expression:" 
-                   (reverse (cons x s)))])))
-
-
-
-(define nos (list 100 50 10 6 5 1))
-(permutations nos)
-(define ops (list '+ '- '/ '*))
-(cartesian-product ops ops ops ops ops);groups operators
-#|
-
-;Function to make a perm into a rpn expression and calculates the expression if valid rpn
-(define (to-rpn l)
-  (if(valid-rpn? (append nos l ops))
-     (calculate-rpn(nos l ops))
-    #f)
-  )
-  
 |#
-
-
-
